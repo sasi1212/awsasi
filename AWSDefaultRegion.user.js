662b9b9bda853a6058fee84bfed5022b3d6271c1
sasi1212 <sasi1212@users.noreply.github.com>
2020-01-11 01:49:24 +0530

    Update AWSDefaultRegion.user.js

--- AWSDefaultRegion.user.js
+++ AWSDefaultRegion.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         AWSDefaultRegion
-// @version      1.1
+// @version      1.2
 // @author       Sasikumar K
 // @source       https://github.com/sasi1212/awsasi/raw/master/AWSDefaultRegion.user.js
 // @namespace    aws.amazon.com
